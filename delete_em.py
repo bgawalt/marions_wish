693dc921da4b388a53f65eaa24587cfb80599b57
Brian Gawalt <bgawalt@gmail.com>
2020-04-14 22:57:26 -0700

    Print response json on delete failure

--- delete_em.py
+++ delete_em.py
@@ -9,6 +9,7 @@ Usage:
     python delete_em.py config.txt
 """
 
+import json
 import requests
 import sys
 import time
@@ -20,12 +21,7 @@ from requests_oauthlib import OAuth1
 
 
 DELETE_ME = [
-    (Texter.TIM, '1249944035772354561'),
-    (Texter.GREGG, '1249944049072476160'),
     (Texter.MARK, '1249944062288752640'),
-    (Texter.TIM, '1249944093666316288'),
-    (Texter.TIM, '1249944169113477121'),
-    (Texter.GREGG, '1249944194929405952')
 ]
 
 
@@ -33,12 +29,16 @@ def main(argv):
     config = parse_config(argv[1])
     oauths = create_sender_oauths(config)
     for sender, tweet_id_str in DELETE_ME:
-        print('Attempting', sender, tweet_id_str)
+        print('\nAttempting', sender, tweet_id_str)
         resp = requests.post(
-            url=("https://api.twitter.com/1.1/statuses/destroy/%s.json" %
+            url=('https://api.twitter.com/1.1/statuses/destroy/%s.json' %
                  (tweet_id_str,)),
             auth=oauths[sender])
-        print('    deleted', resp.json().get('id_str', None))
+        if 'id_str' in resp.json():
+            print('   delete succeeded')
+        else:
+            print('FAILURE!')
+            print(json.dumps(resp.json(), indent=2))
         time.sleep(10)
 
 
